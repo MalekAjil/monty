0015e11608c07660181540debd6d98ea10b2e281
MalekAjil <aboanasajil@gmail.com>
2023-12-17 17:26:34 -0800

    push/pall

--- pint.c
+++ pint.c
@@ -11,11 +11,13 @@ void pint(stack_t **stack, unsigned int line_number)
 	stack_t *s = *stack;
 
 	if (s == NULL)
-		return;
+	{
+		dprintf(2, "L%d: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
 	while (s->next != NULL)
 	{
 		s = s->next;
 	}
 	printf("%d\n", s->n);
-	line_number++;
 }
